8e7f0b5b7ce5000e7894d12ad08f608c9b6ed1ac
windowdong11 <windowdong11@naver.com>
2021-12-01 09:21:04 +0900

    Fix: 단순 오탈자 수정

--- src/apis/searchAudio.js
+++ src/apis/searchAudio.js
@@ -10,7 +10,7 @@ export const searchAudio = async (keyword, page = 1) => {
         response = await axios.get(`https://api.pukuba.dev/api/v1/audio/search`, {
             headers: {
                 "Content-Type": "application/json",
-                Authroization: `Bearer ${localStorage.getItem("token")}`,
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
             },
             params: {
                 keyword: keyword,
@@ -23,3 +23,4 @@ export const searchAudio = async (keyword, page = 1) => {
 
     return response.data
 }
+// 리액트 안돌아가서 오디오서치좀 삭제함
