755236378fafe3a45afe0a8ffff13964e48100ac
windowdong11 <windowdong11@naver.com>
2021-12-01 09:59:42 +0900

    음악 페이지 이동 버튼 추가

--- src/components/ContentBody.jsx
+++ src/components/ContentBody.jsx
@@ -1,11 +1,20 @@
 import React, {useEffect, useRef} from 'react';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import { ChevronRight } from '@mui/icons-material';
 
 export default (props) => {
     // TODO : props 에 포함될 것들 [Title, li]
-    const { title, Component, options } = props;
+    const { title, Component, options, onclickPrevBtn, onclickNextBtn } = props;
     return (
         <div className="content-section">
-            <div className="content-section-title">{title}</div>
+            <div className="content-section-title">{title}
+            <div className='content-section-btn'>
+                {onclickPrevBtn
+                    && <ChevronLeftIcon className="btn btn-prev" onClick={onclickPrevBtn}/>}
+                {onclickNextBtn
+                 && <ChevronRight className="btn btn-next" onClick={onclickNextBtn}/>}
+            </div>
+            </div>
             <ul>
                 {Component && options?.map((option, index) => <Component key={index} {...option} />)}
             </ul>
--- src/screens/Music.jsx
+++ src/screens/Music.jsx
@@ -5,6 +5,9 @@ import axios from "axios"
 
 export default ({ sortOption }) => {
     const [songs, setSongs] = useState([])
+    
+    const handlePrevButton = () => {}
+    const handleNextButton = () => {}
     useEffect(() => {
         axios
             .get("https://api.pukuba.dev/api/v1/audio/random")
@@ -25,5 +28,5 @@ export default ({ sortOption }) => {
             })
             .catch((err) => console.log(err.response.data))
     }, [])
-    return <ContentBody Component={SongInformation} options={songs} />
+    return <ContentBody Component={SongInformation} options={songs} onclickPrevBtn={handlePrevButton} onclickNextBtn={handleNextButton}/>
 }
