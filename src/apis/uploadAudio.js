60f5c2a44f3bc61093a102525d9927e391a740f8
SeungWon <20sunrin041@sunrint.hs.kr>
2021-12-02 11:45:39 +0900

    feat: Add event after audio upload

--- src/apis/uploadAudio.js
+++ src/apis/uploadAudio.js
@@ -1,42 +1,37 @@
 import axios from "axios"
 
-
-
 const isValidFilter = (filter) => {
     const filters = ["Default", "NightCore", "NoiseFilter", "Stereo"]
     return filters.includes(filter)
 }
 
 /**
- * @param {string} youtubeLink 음원의 유튜브 주소 
+ * @param {string} youtubeLink 음원의 유튜브 주소
  * @param {string} filter 정제 옵션 Default | NightCore | NoiseFilter | Stereo
  * @param {string} name 음원이 이름 undefined면 자동으로 유튜브 타이틀이 됨
  */
-const uploadAudio = async( youtubeLink, filter = "Default", name) => {
-
-    if(!isValidFilter(filter)){
+const uploadAudio = async (youtubeLink, filter = "Default", name) => {
+    if (!isValidFilter(filter)) {
         throw new Error("필터가 올바르지 않습니다")
     }
 
     const data = {
         youtubeLink,
         filter,
-        name
+        name,
     }
     let response
-    console.log(data)
-        console.log(localStorage.getItem("token"))
-        
+
     try {
-        response = await axios("https://api.pukuba.dev/api/v1/audio/link",{
-            method:"POST",
+        response = await axios("https://api.pukuba.dev/api/v1/audio/link", {
+            method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "Authorization": `Bearer ${localStorage.getItem("token")}`
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
             },
-            data
+            data,
         })
-    } catch(e) {
+    } catch (e) {
         throw new Error(e.message)
     }
     return response.data
--- src/screens/MusicUpload.jsx
+++ src/screens/MusicUpload.jsx
@@ -1,26 +1,46 @@
-import React from "react";
-import ContentBody from "../components/ContentBody";
-import axios from "axios";
-import uploadAudio from "../apis/uploadAudio";
-import { useAuth } from "../hooks/Auth";
-import { useNavigate } from "react-router-dom";
+import React from "react"
+import ContentBody from "../components/ContentBody"
+import axios from "axios"
+import uploadAudio from "../apis/uploadAudio"
+import { useAuth } from "../hooks/Auth"
+import { useNavigate } from "react-router-dom"
+import Swal from "sweetalert2"
 
-
-            
 const musicFilters = ["Default", "NightCore", "NoiseFilter", "Stereo"]
 const MusicUpload = () => {
-    const [url, setURL] = React.useState('');
-    const [musicFilter, setMusicFilter] = React.useState('Default');
-    const [musicName, setMusicName] = React.useState('');
+    const [url, setURL] = React.useState("")
+    const [musicFilter, setMusicFilter] = React.useState("Default")
+    const [musicName, setMusicName] = React.useState("")
     const auth = useAuth()
     const navigator = useNavigate()
     const handleUpload = async () => {
-        if(auth.isSignedOut()) navigator('/signin')
+        if (auth.isSignedOut()) {
+            Swal.fire("로그인을 해야합니다")
+            return navigator("/signin")
+        }
+        const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 30000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener("mouseenter", Swal.stopTimer)
+                toast.addEventListener("mouseleave", Swal.resumeTimer)
+            },
+        })
+
+        Toast.fire({
+            icon: "success",
+            title: "음원 업로드중...",
+        })
         try {
             const result = await uploadAudio(url, musicFilter, musicName)
+            Swal.fire(`음원이 정상적으로 업로드되었습니다`)
+            navigator("/")
             console.log(result)
-        } catch(e) {
-            console.log(e.response)
+        } catch (e) {
+            Swal.fire(`음원을 업로드하는도중 오류가 발생했습니다`)
         }
     }
     return (
@@ -29,37 +49,53 @@ const MusicUpload = () => {
             <ul>
                 <li className="adobe-product">
                     <div className="products short">Music Name</div>
-                    <input type="text" value={musicName} onChange={(e) => setMusicName(e.target.value)} placeholder="music name"></input>
+                    <input
+                        type="text"
+                        value={musicName}
+                        onChange={(e) => setMusicName(e.target.value)}
+                        placeholder="music name"
+                    ></input>
                     <div className="button-wrapper">
                         <button
                             className="content-button status-button open"
                             onClick={handleUpload}
                         >
-                        Upload
+                            Upload
                         </button>
                     </div>
                 </li>
                 <li className="adobe-product">
                     <div className="products short">URL</div>
-                    <input type='url' value={url} onChange={e => setURL(e.target.value)} placeholder="URL here"></input>
+                    <input
+                        type="url"
+                        value={url}
+                        onChange={(e) => setURL(e.target.value)}
+                        placeholder="URL here"
+                    ></input>
                 </li>
                 <li className="adobe-product">
                     <div className="products short">Music Filters</div>
-                    {musicFilters.map((filter, index) => 
-                    <div>
-                        <input type="radio" name='music-filters' key={index} id={filter}
-                        style={{marginRight: "5px"}}
-                        value={filter} checked={musicFilter === filter}
-                        onChange={(e) => setMusicFilter(e.target.value)}/>
-                        <label htmlFor={filter}
-                        style={{marginRight: "8px"}}
-                        ><i>{filter}</i></label>
-                    </div>
-                    )}
+                    {musicFilters.map((filter, index) => (
+                        <div>
+                            <input
+                                type="radio"
+                                name="music-filters"
+                                key={index}
+                                id={filter}
+                                style={{ marginRight: "5px" }}
+                                value={filter}
+                                checked={musicFilter === filter}
+                                onChange={(e) => setMusicFilter(e.target.value)}
+                            />
+                            <label htmlFor={filter} style={{ marginRight: "8px" }}>
+                                <i>{filter}</i>
+                            </label>
+                        </div>
+                    ))}
                 </li>
             </ul>
         </div>
     )
 }
 
-export default MusicUpload;
+export default MusicUpload
