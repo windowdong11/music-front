d2ece2103f86ea6f858a3251a2b215493877ec63
windowdong11 <windowdong11@naver.com>
2021-12-20 03:59:15 +0900

    chore: ContentBody 우측상단에 추가메뉴를 구성할 수 있도록 변경

--- src/components/ContentBody.jsx
+++ src/components/ContentBody.jsx
@@ -1,19 +1,24 @@
-import React, {useEffect, useRef} from 'react';
+import React, { useEffect, useRef } from 'react';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import { ChevronRight } from '@mui/icons-material';
 
 export default (props) => {
     // TODO : props 에 포함될 것들 [Title, li]
-    const { title, Component, options, onclickPrevBtn, onclickNextBtn } = props;
+    const { title, Component, options, onclickPrevBtn, onclickNextBtn, RightMenuComponent } = props;
     return (
-        <div className="content-section">
-            <div className="content-section-title">{title}
-            <div className='content-section-btn'>
-                {onclickPrevBtn
-                    && <ChevronLeftIcon className="btn btn-prev" onClick={onclickPrevBtn}/>}
-                {onclickNextBtn
-                 && <ChevronRight className="btn btn-next" onClick={onclickNextBtn}/>}
-            </div>
+        <div className="content-section" >
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <div className="content-section-title">{title}
+                    <div className='content-section-btn'>
+                        {onclickPrevBtn
+                            && <ChevronLeftIcon className="btn btn-prev" onClick={onclickPrevBtn} />}
+                        {onclickNextBtn
+                            && <ChevronRight className="btn btn-next" onClick={onclickNextBtn} />}
+                    </div>
+                </div>
+                <div>
+                    {RightMenuComponent}
+                </div>
             </div>
             <ul>
                 {Component && options?.map((option, index) => <Component key={index} {...option} />)}
